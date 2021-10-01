2fd298e35d85a8c7dfe3c9832054151816ffc606
Andrew Gravino <aggravino@cpp.edu>
2021-10-01 14:47:10 -0700

    Create guidelines for coding

---
+++ magicSquare.c
@@ -0,0 +1,16 @@
+//Gravino, Andrew
+//CS2600, In Class Coding Assignment 2
+//10-1-21
+
+/*
+1. Hard code an array initialization and definition that will be evaluted by Lo Shu Magic Square if conditions
+2. The if conditions will assign certain truth values to booleans initialized at the beginning
+3. Nearing the end of the file, the booleans will all be checked and it will print out whether or not the provided array was a magic square or not
+*/
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+
+    return EXIT_SUCCESS;
+}
