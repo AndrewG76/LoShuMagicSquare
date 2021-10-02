2eb811084f5185cbe92b04aa84dd459deb1ef31a
Andrew Gravino <aggravino@cpp.edu>
2021-10-01 17:08:55 -0700

    Finished all conditions, left for debugging now

--- magicSquare.c
+++ magicSquare.c
@@ -8,20 +8,20 @@
 3. Nearing the end of the file, the booleans will all be checked and it will print out whether or not the provided array was a magic square or not
 */
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 int main(){
     bool rows15 = false; //done
     bool columns15 = false; //done
     bool diagonals15 = false; //done
-    bool containsAll = true; //not done
-    bool noRepeats = true; //done
+    bool noRepeats = true; //needs fixing
     bool betweenOneAndNine = true; //done
 
     int tableInput[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
+        {2, 7, 6},
+        {9, 5, 1},
+        {4, 3, 8}
     };
 
     int checkerArray[10] = {}; //this declaration and initialization will be used for comparison later down the line
@@ -29,11 +29,9 @@ int main(){
     for(int rowIndex = 0; rowIndex < 3; rowIndex++){
         for(int columnIndex = 0; columnIndex < 3; columnIndex++){
             checkerArray[checkerIndex] = tableInput[rowIndex][columnIndex];
+            checkerIndex++; //"iterating" through our empty array
         }
-        checkerIndex++; //way of "iterating" through our empty array
     }
-
-    int oneThroughNine[10] = {0,1,2,3,4,5,6,7,8,9}; 
 
     for(int rowIndex = 0; rowIndex < 3; rowIndex++){
         for(int columnIndex = 0; columnIndex < 3; columnIndex++){
@@ -44,10 +42,18 @@ int main(){
         }
     }   
 
-    for(int firstOneDIndex = 0; firstOneDIndex < 9; firstOneDIndex++){ //these go up to 9 instead of 10 because
-        for(int secondOneDIndex = firstOneDIndex + 1; secondOneDIndex < 9; secondOneDIndex++){ //secondOneDIndex is + 1 each time to increment through and there's no need to compare to items before it since we already do that
+    //if your inputs are forced to be between 1-9 and there are no repeats allowed, then you will be forced to use all 9 unique numbers
+    
+    /* see what the copied array looks like
+    for(int i = 0; i < 9; i++){
+        printf("%d", checkerArray[i]);
+    }
+    */
+
+    for(int firstOneDIndex = 0; firstOneDIndex < 9; firstOneDIndex++){ //index 0-8 for the loops because we can't check the end with the one ahead of it
+        for(int secondOneDIndex = firstOneDIndex + 1; secondOneDIndex < 9; secondOneDIndex++){ //secondOneDIndex is + 1 each time to increment through and there's no need to compare to items before it since we already did that prior
             if(checkerArray[firstOneDIndex] == checkerArray[secondOneDIndex]){
-                noRepeats = false; //if we detect that the second index finds anything equivalent 
+                noRepeats = false; //if we detect that the second index finds any repeats, the boolean becomes false 
                 break;
             }
         }
@@ -73,18 +79,18 @@ int main(){
         diagonals15 = true;
     }
 
-    printf("This is what your array looks like:\n"); //printing our hard coded input
+    printf("This is what your array looks like:\n"); //printing our hard coded input in the 3 by 3 format requested
     for(int rowIndex = 0; rowIndex < 3; rowIndex++){
-        printf("\n");
         for(int columnIndex = 0; columnIndex < 3; columnIndex++){
-            printf(tableInput[rowIndex][columnIndex]);
+            printf("%d", tableInput[rowIndex][columnIndex]);
         }
+        printf("\n");
     }
-    if(rows15 && columns15 && diagonals15){
-        printf("\nThe array is a magic square!"); //printing final evaluations
+    if(rows15 && columns15 && diagonals15 && noRepeats && betweenOneAndNine){
+        printf("The array is a magic square!"); //printing final evaluations
     }
     else{
-        printf("\nThe array is not a magic square.");
+        printf("The array is not a magic square.");
     }
 
     return EXIT_SUCCESS;
